4b4122bf06b3d4b36186fc84f2a0a92db5ad5119
Moon <1tjen@naver.com>
2023-09-14 02:43:11 +0900

    feat: OpenWeatherAPI 생성

---
+++ Diary/Model/API/Implementation/OpenWeatherAPI.swift
@@ -0,0 +1,22 @@
+//
+//  OpenWeatherAPI.swift
+//  Diary
+//
+//  Created by 조호준 on 2023/09/14.
+//
+
+import Foundation
+
+struct OpenWeatherAPI: APIType {
+    var baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    var queryItems: [URLQueryItem]?
+    var apiKey = Bundle.openWeatherAPIKey
+    
+    init(latitude: String, longitude: String) {
+        queryItems = [
+            URLQueryItem(name: "lat", value: latitude),
+            URLQueryItem(name: "lon", value: longitude),
+            URLQueryItem(name: "appid", value: apiKey)
+        ]
+    }
+}
